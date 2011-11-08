8c41c8b801caf5d7877f774e4621f9d43fc95af6
Luca Orlandi <luca.orlandi@gmail.com>
2011-11-08 19:17:19 +0100

    my first commit

---
+++ src/main/java/com/github/seoapp/HomeController.java
@@ -0,0 +1,54 @@
+package com.github.seoapp;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+
+	private static final Logger logger = LoggerFactory
+			.getLogger(HomeController.class);
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! the client locale is " + locale.toString());
+
+		fillModel(locale, model);
+
+		return "home";
+	}
+
+	private void fillModel(Locale locale, Model model) {
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
+				DateFormat.LONG, locale);
+
+		String formattedDate = dateFormat.format(date);
+
+		model.addAttribute("serverTime", formattedDate);
+		model.addAttribute("currentLocale", locale.toString());
+	}
+
+	public String privatePage(Locale locale, Model model) {
+		logger.info("This is a private page the client locale is "
+				+ locale.toString());
+
+		fillModel(locale, model);
+		return "secret";
+	}
+
+}
