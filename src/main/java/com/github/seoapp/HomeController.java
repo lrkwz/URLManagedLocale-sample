aa9406651e16805375da50a3d6ef081cb6aaedf6
Luca Orlandi <luca.orlandi@gmail.com>
2011-11-11 13:02:11 +0100

    Injected security framework

--- src/main/java/com/github/seoapp/HomeController.java
+++ src/main/java/com/github/seoapp/HomeController.java
@@ -4,12 +4,15 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.support.RequestContextUtils;
 
 /**
  * Handles requests for the application home page.
@@ -24,23 +27,30 @@ public class HomeController {
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! the client locale is " + locale.toString());
+	public String home(HttpServletRequest request, Model model) {
+		logger.info(
+				"Welcome home! the client locale is {}",
+				RequestContextUtils.getLocale(request) != null ? RequestContextUtils
+						.getLocale(request).toString() : " not set");
 
-		fillModel(locale, model);
+		fillModel(RequestContextUtils.getLocale(request), model);
 
 		return "home";
 	}
 
 	private void fillModel(Locale locale, Model model) {
 		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG,
-				DateFormat.LONG, locale);
+		DateFormat dateFormat = locale != null ? DateFormat
+				.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale)
+				: DateFormat.getDateTimeInstance(DateFormat.LONG,
+						DateFormat.LONG);
 
 		String formattedDate = dateFormat.format(date);
 
 		model.addAttribute("serverTime", formattedDate);
-		model.addAttribute("currentLocale", locale.toString());
+		if (locale != null) {
+			model.addAttribute("currentLocale", locale.toString());
+		}
 	}
 
 	public String privatePage(Locale locale, Model model) {
--- src/main/webapp/WEB-INF/views/home.jsp
+++ src/main/webapp/WEB-INF/views/home.jsp
@@ -1,14 +1,40 @@
-<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
-<%@ page session="false" %>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ page session="false"%>
 <html>
 <head>
-	<title>Home</title>
+<title>Home</title>
+</head>
+<title>Home</title>
 </head>
 <body>
-<h1>
-	Hello world!  
-</h1>
+	<h2>Info</h2>
+	<p>The time on the server is ${serverTime}.</p>
+	<p>Your locale is ${currentLocale }</p>
 
-<P>  The time on the server is ${serverTime}. </P>
+	<h2>Test suite</h2>
+	<ol>
+		<li><a href="./">Unqualified URL</a> (*) this should show the cookie stored locale or redirect to the "choose locale" page if no cookies are set</li>
+		<li><a href="./it_IT">Italian</a> change locale to the specified value, sets cookie if not set</li>
+		<li><a href="./en_GB">British English</a> (idem)</li>
+		<li><a href="./xx_XX">Unknown</a> change locale to default jvm</li>		
+	</ol>
+	
+	<h2>Special <i>pages</i></h2>
+	<ol>
+		<li><a href="secure/">Your account</a> (**)</li>
+		<li><a href="secure/extreme/">Admin</a> (**)</li>
+		<li><a href="changelocale.html">Change locale page</a></li>
+	</ol>
+	
+	<blockquote>(*) Open links also in <a href="http://en.wikipedia.org/wiki/Privacy_mode" target="help">private/incognito</a> mode in order to test without cookies!</blockquote>
+	<blockquote>
+	    (**) Usernames/Passwords are:
+	    <ul>
+        <li>rod/koala (admin)</li>
+        <li>dianne/emu</li>
+        <li>scott/wombat</li>
+        <li>peter/opal</li>
+        </ul>
+	</blockquote> 
 </body>
 </html>
